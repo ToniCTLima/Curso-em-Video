2aa45b5c839f4f9c90eb916a893705f8a2fce872
Toni <tonictlima@gmail.com>
2024-10-30 20:27:41 -0300

    ex.002.py
    Crie um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas.

---
+++ ex002.py
@@ -0,0 +1,4 @@
+# Crie um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas.
+
+nome = input('Digite seu nome: ')
+print('Olá {}, seja bem vindo ao mundo da programação.'.format(nome))
