1533ae2d62a4f2ab56371e1f539dec0db934589f
Toni <tonictlima@gmail.com>
2024-11-03 12:08:16 -0300

    ex009.py
    Faça um programa que leia um número inteiro qualquer e mostre na tela sua tabuada.

---
+++ ex009.py
@@ -0,0 +1,24 @@
+# Faça um programa que leia um número inteiro qualquer e mostre na tela sua tabuada.
+
+n1 = int(input('Digite um número para ver sua tabuada: '))
+r1 = n1 * 1
+r2 = n1 * 2
+r3 = n1 * 3
+r4 = n1 * 4
+r5 = n1 * 5
+r6 = n1 * 6
+r7 = n1 * 7
+r8 = n1 * 8
+r9 = n1 * 9
+r10 = n1 * 10
+print('A tabuada do número {} é:'.format(n1))
+print('{} x {} = {}'.format(n1, 1, r1))
+print('{} x {} = {}'.format(n1, 2, r2))
+print('{} x {} = {}'.format(n1, 3, r3))
+print('{} x {} = {}'.format(n1, 4, r4))
+print('{} x {} = {}'.format(n1, 5, r5))
+print('{} x {} = {}'.format(n1, 6, r6))
+print('{} x {} = {}'.format(n1, 7, r7))
+print('{} x {} = {}'.format(n1, 8, r8))
+print('{} x {} = {}'.format(n1, 9, r9))
+print('{} x {} = {}'.format(n1, 10, r10))
