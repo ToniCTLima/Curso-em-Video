4fd970b3388c54f7681ffb0ff8f11eff4e8a59bc
Toni <tonictlima@gmail.com>
2024-11-07 22:10:17 -0300

    ex015.py
    Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado e a quantidade de dias pelo quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$ 60 por dia e R$ 0.15 por km rodado.

---
+++ ex015.py
@@ -0,0 +1,3 @@
+# Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado e a quantidade de dias pelo quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$ 60 por dia e R$ 0.15 por km rodado.
+
+km = float(input('Digite a a quilometragem rodada: '))
