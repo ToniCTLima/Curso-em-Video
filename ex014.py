46b3915be7044a7093a500b63f0cef53b144c7b6
Toni <tonictlima@gmail.com>
2024-11-11 22:47:17 -0300

    ex014.py

--- ex014.py
+++ ex014.py
@@ -1,5 +1,5 @@
 # Escreva um programa que converta uma temperatura digitada em C° e converta para F°.
 
 cel = float(input('Digite a tempertura em graus C° para converter em F°: '))
-c_f = 9 * cel / 5 + 32
+c_f = ((9 * cel) / 5) + 32
 print('A temperatura de {}c°, corresponde a {}f°'.format(cel, c_f))
