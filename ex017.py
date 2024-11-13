bcbe986455103c54110ece3e8d780b7b492a6bdf
Toni <tonictlima@gmail.com>
2024-11-13 19:25:09 -0300

    ex017.py
    Crie um programa que leia o comprimento do cateto adjacente e do cateto oposto de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa.

--- ex017.py
+++ ex017.py
@@ -1,7 +1,6 @@
 # Crie um programa que leia o comprimento do cateto adjacente e do cateto oposto de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa.
 
 # sem importar math
-
 co = float(input('Comprimento do cateto oposto: '))
 ca = float(input('Comprimento do cateto adjacente: '))
 hi = (co ** 2 + ca ** 2) ** (1/2)
