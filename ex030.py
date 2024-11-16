9f844c84cab30fc08aedbc20f375cad074c4feea
Toni <tonictlima@gmail.com>
2024-11-16 12:23:24 -0300

    Update ex030.py
    Adicionado a resolução do professor.

--- ex030.py
+++ ex030.py
@@ -5,3 +5,13 @@ if numero % 2 == 0 :
     print(f'O número {numero} é par')
 else:
     print(f'O número {numero} é ímpar')
+
+
+# Resolução do professor
+
+numero = int(input('Me diga um número qualquer: '))
+resultado = numero % 2
+if resultado == 0:
+    print('O número {} é PAR'.format(numero))
+else:
+    print('O número {} é ÍMPAR'.format(numero))
