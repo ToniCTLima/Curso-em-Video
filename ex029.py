a176c7c142aa10eda23338d77164da606ef1b4be
Toni <tonictlima@gmail.com>
2024-11-16 10:43:44 -0300

    Update ex029.py
    Update do exercício com a resolução do professor.

--- ex029.py
+++ ex029.py
@@ -9,9 +9,17 @@ multa = excesso_velo * 7
 # MULTA
 print(velo)
 if velo > 80:
-    #excesso_velo = velo - 80
-    #multa = excesso_velo * 7
-    print(f'Você foi multado, sua velocidade era de {velo} km/h')
+    print(f'Você foi multado, sua velocidade era de {velo} km/h. O limite é de 80 km/h.')
     print(f'O valor da multa é de R$ {multa}')
 else:
     print('Você está dentro dos limites de velocidade.')
+
+# Solução do Professor
+
+velocidade = float(input('Qual a velocidade do carro? '))
+if velocidade > 80:
+    print('MULTADO! Você excedeu o limite permitido que é de 80 km/h')
+    multa = (velocidade - 80) * 7
+    print('Você deve pagar uma multa de R$ {:.2f}'.format(multa))
+
+print('Tenha um bom dia! Dirija com segurança.')
