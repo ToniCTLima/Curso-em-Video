a85f38535be0faa14d4385458d214602ea4893ba
Toni <tonictlima@gmail.com>
2024-11-18 21:20:22 -0300

    Update ex001.py
    Inserido padrão ANSI, para colocar algumas cores no terminal

--- ex001.py
+++ ex001.py
@@ -1,3 +1,8 @@
 # Exercício 001 - Crie um programa que escreva 'Olá Mundo' na tela.
 
-print('Olá Mundo')
+# Padrão ANSI para colocar cor no terminal \033[0;31;40m
+# Códigos para estilo (style): 0, 1, 4, 7 - 0 sem estilo, 1 negrito, 4 sublinado, 7 inverte as configurações
+# Códigos para cor no texto: 30 branco, 31 vermelho, 32 verde, 33 amarelo, 34 azul, 35 roxo, 36 ciano, 37 cinza.
+# Códigos para cor de background: 40 até 47 com a mesma sequência de cores do texto.
+
+print('\033[1;35mOlá \033[m\033[34mMundo\033[m') 
